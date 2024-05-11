b0037aa15d5714fa2a45c1d88c60ac9e3b2ed873
Rohit kumar <119152906+rohitkumar0703@users.noreply.github.com>
2024-05-11 23:24:52 +0530

    commit

---
+++ Arrays/Assignment/Question_one.java
@@ -0,0 +1,35 @@
+package Arrays.Assignment;
+// Question 1:Given an integer array nums,return true if any value appears 
+// at least twice in the array, and return false if every element is distinct.
+// 
+// Example 1:Input:nums = [1, 2, 3, 1]
+//Output:  true 
+
+// Example 2:Input:nums = [1, 2, 3, 4]
+// Output: false 
+// Example 3:Input:nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+// Output:   true 
+// Constraints:
+// •1 <= nums .lengtth <= 105•-109 <= nums [ i ] <= 109 
+
+public class Question_one {
+    public static boolean find_distinct(int arr[]){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] == arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // int arr[] = {1,2,3,1};
+        //int arr[] = {1, 2, 3, 4};
+        int arr[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(find_distinct(arr));
+    }
+}
+// Time Complexity is BigO(n^2)
+
--- Arrays/BuyandSell.java
+++ Arrays/BuyandSell.java
@@ -17,7 +17,7 @@ public class BuyandSell {
             }
         }
         return maxProfit;
-    }
+    } 
 
     public static void main(String[] args) {
         int prices[] = {7, 1, 5, 3, 6, 4};
